0e4e03a95df033e57b4c75427a6dd0323df8aca8
f4t1hsalih <f4t1hsalih@gmail.com>
2023-08-29 19:46:50 +0300

    Add project files.

---
+++ RSS_Ile_Haber_Basliklari_Cekme/Form1.cs
@@ -0,0 +1,10 @@
+namespace RSS_Ile_Haber_Basliklari_Cekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
